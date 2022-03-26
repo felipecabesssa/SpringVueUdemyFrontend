97df5041ffe8b86b50e5f1416c543a65a847b9d2
felipecabesssa <felipesoares100@hotmail.com>
2022-03-26 11:55:26 -0300

    Importando Bootstrap - da forma certa e atual

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <img src="./../assets/trade.png">
+    <div class="items">
+      <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+        <div class="card-header">Categorias</div>
+        <div class="card-body">
+          <h5 class="card-title">Categories</h5>
+          <p class="card-text">lista as categorias</p>
+        </div>
+      </div>
+      <div class="card bg-light mb-3" style="max-width: 18rem;">
+        <div class="card-header">Produtos</div>
+        <div class="card-body">
+          <h5 class="card-title">Products</h5>
+          <p class="card-text">lista dos produtos</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,3 +29,13 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home{
+  display: flex;
+  justify-content: space-around;
+  .items{
+    max-width: 50%;
+  }
+}
+</style>
