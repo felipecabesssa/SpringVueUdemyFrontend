22f7b90753919a01c1dbbf2dda35c7728306f73b
felipecabesssa <felipesoares100@hotmail.com>
2022-03-26 12:47:55 -0300

    Consumindo dados mokados - v-for e v-if para exibicao

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,18 @@
   <div class="home">
     <img src="./../assets/trade.png">
     <div class="items">
-      <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+      <div class="card text-white bg-info mb-3" style="max-width: 90%;">
         <div class="card-header">Categorias</div>
         <div class="card-body">
           <h5 class="card-title">Categories</h5>
-          <p class="card-text">lista as categorias</p>
+          <p class="card-text">Os diversos tipos de Produto são divididos em categorias especificas</p>
         </div>
       </div>
-      <div class="card bg-light mb-3" style="max-width: 18rem;">
+      <div class="card bg-light mb-3" style="max-width: 90%;">
         <div class="card-header">Produtos</div>
         <div class="card-body">
           <h5 class="card-title">Products</h5>
-          <p class="card-text">lista dos produtos</p>
+          <p class="card-text">Você pode acrescentar no tipo de produto, ou acrescentar, atualizar e deletar produto já existente.</p>
         </div>
       </div>
     </div>
@@ -34,6 +34,11 @@ export default {
 .home{
   display: flex;
   justify-content: space-around;
+  img{
+    padding-left: 15px;
+    max-width: 45%;
+    height: 350px;
+  }
   .items{
     max-width: 50%;
   }
